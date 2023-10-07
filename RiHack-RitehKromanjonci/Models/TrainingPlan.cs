08227de9178fa5ba8edef5f8a4adba0ac9275f7b
dini777 <karlojakopov@gmail.com>
2023-10-07 14:10:07 +0200

    comit

--- RiHack-RitehKromanjonci/Models/TrainingPlan.cs
+++ RiHack-RitehKromanjonci/Models/TrainingPlan.cs
@@ -8,4 +8,11 @@ public class TrainingPlan
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
+    public int TimesOfTrainingPerWeek { get; set; }
+    public int UsersGymExpInYears { get; set; }
+    public string? LoseOrGainMass { get; set; }
+    public List<string> CardioExercises { get; set; } = new List<string>();
+    public List<string> GymExercises { get; set; } = new List<string>();
+    public bool IsQuestionaireFilled { get; set; } = false;
+
 }
