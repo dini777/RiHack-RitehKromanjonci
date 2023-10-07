ff5aa67de4274a45c0352825ad21b870ba276dfa
dini777 <karlojakopov@gmail.com>
2023-10-07 11:05:58 +0200

    Setupan database

--- RiHack-RitehKromanjonci/Program.cs
+++ RiHack-RitehKromanjonci/Program.cs
@@ -1,13 +1,19 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 using RiHack_RitehKromanjonci.Data;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
