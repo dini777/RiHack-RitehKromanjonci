a7eacc15b02545baa909daecfcdd29ed3678ce1a
Krapic <krapicf@gmail.com>
2023-10-08 01:03:32 +0200

    novi login page

---
+++ RiHack-RitehKromanjonci/Pages/login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RiHack_RitehKromanjonci.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
