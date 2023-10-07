0b112cfa1889d0a1090206c60c24bbd6b727930e
dini777 <karlojakopov@gmail.com>
2023-10-07 14:46:24 +0200

    quick fix

--- RiHack-RitehKromanjonci/Models/User.cs
+++ RiHack-RitehKromanjonci/Models/User.cs
@@ -8,15 +8,7 @@ public class User
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
-    public int TrainingPlanId { get; set; }
-    public int EatingPlanId { get; set; }
-    public int DailyGoalId { get; set; }
-    public int YourProgressId { get; set; }
     public string Email { get; set; } = string.Empty;
     public string Username { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
-    public TrainingPlan? TrainingPlans { get; set; }
-    public EatingPlan? EatingPlans { get; set; }
-    public DailyGoal? DailyGoals { get; set; }
-    public YourProgress? Progress { get; set; }
 }
