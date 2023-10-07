d99d700c8bd80da956cf26c9f9c90797caf041b3
dini777 <karlojakopov@gmail.com>
2023-10-07 16:43:37 +0200

    Rijesena mapa - post

--- RiHack-RitehKromanjonci/Program.cs
+++ RiHack-RitehKromanjonci/Program.cs
@@ -10,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
+builder.Services.AddHttpContextAccessor();
+
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
---
+++ RiHack-RitehKromanjonci/wwwroot/JavaScriptLocation.js
@@ -0,0 +1,8 @@
+﻿window.getLocation = function (dotnetHelper) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+
+        dotnetHelper.invokeMethodAsync('OnLocationReceived', latitude, longitude);
+    });
+}
