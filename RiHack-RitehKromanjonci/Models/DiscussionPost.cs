3b907c25ba93e4d5a0a76ebc658646b434951327
dini777 <karlojakopov@gmail.com>
2023-10-08 03:56:52 +0200

    Fixevi sa ulogiranim userima

--- RiHack-RitehKromanjonci/Models/DiscussionPost.cs
+++ RiHack-RitehKromanjonci/Models/DiscussionPost.cs
@@ -7,6 +7,7 @@ public class DiscussionPost
     public string Content { get; set; }
     public string Author { get; set; }
     public DateTime DatePosted { get; set; }
+    //public User User { get; set; }
 
     public List<Reply> Replies { get; set; } = new List<Reply>();
 }
