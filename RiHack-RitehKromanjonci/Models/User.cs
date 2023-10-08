486c4c193c65703c0306a636be6398fcc882d8a6
dini777 <karlojakopov@gmail.com>
2023-10-08 05:05:32 +0200

    Fixevi na userima

--- RiHack-RitehKromanjonci/Models/User.cs
+++ RiHack-RitehKromanjonci/Models/User.cs
@@ -14,5 +14,5 @@ public class User
     public int Points { get; set; } = 0;
     public Rank Ranks { get; set; } = Rank.Empty;
     public List<PostModel> Posts { get; set; } = new List<PostModel>();
-    public List<User> Friends { get; set; }
+    public List<User> Friends { get; set; } = new List<User>();
 }
