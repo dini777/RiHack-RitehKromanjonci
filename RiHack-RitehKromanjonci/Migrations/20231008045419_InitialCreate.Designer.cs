18d6ac1108c26b109bcec1b8b4a9958054931c56
dini777 <karlojakopov@gmail.com>
2023-10-08 06:54:50 +0200

    fix-migracija

--- RiHack-RitehKromanjonci/Migrations/20231008045419_InitialCreate.Designer.cs
+++ RiHack-RitehKromanjonci/Migrations/20231008045419_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using RiHack_RitehKromanjonci.Data;
 namespace RiHack_RitehKromanjonci.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20231008032956_InitialCreate")]
+    [Migration("20231008045419_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
