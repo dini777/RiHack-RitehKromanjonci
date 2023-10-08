a6e00bcff878f8d8dd55b2e3980c5526acbc9ef6
Krapic <krapicf@gmail.com>
2023-10-08 02:28:58 +0200

    buvz

---
+++ RiHack-RitehKromanjonci/Pages/register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RiHack_RitehKromanjonci.Pages
+{
+    public class IndexModelReg : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
